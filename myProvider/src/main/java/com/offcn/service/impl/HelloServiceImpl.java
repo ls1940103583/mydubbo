3db76ac9a23f174bca1867ab38952387f9709d26
ls1940103583 <59644115+ls1940103583@users.noreply.github.com>
2020-04-20 14:09:24 +0800

    Update HelloServiceImpl.java

--- myProvider/src/main/java/com/offcn/service/impl/HelloServiceImpl.java
+++ myProvider/src/main/java/com/offcn/service/impl/HelloServiceImpl.java
@@ -3,6 +3,7 @@ package com.offcn.service.impl;
 import com.offcn.HelloService;
 
 public class HelloServiceImpl implements HelloService {
+    //线上修改代码测试
     @Override
     public String sayHello(String name) {
         return name+":welcome!";
